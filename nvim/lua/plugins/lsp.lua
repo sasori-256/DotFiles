a150c88a9c62297f4c821f950bffaf1024ed38fe
sasori-256 <sasori.0274@gmail.com>
2025-09-01 17:44:40 +0900

    refactor(nvim): Simplify lsp and treesitter setup

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -8,8 +8,6 @@ return {
         "luacheck",
         "shellcheck",
         "shfmt",
-        "tailwindcss-language-server",
-        "css-lsp",
         "dart-debug-adapter",
         "tinymist",
       })
