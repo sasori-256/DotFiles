5521f55f7c7baada890798dc2cdefb029b7e47aa
sasori-256 <Sasori.0274@gmail.com>
2025-09-23 12:38:40 +0900

    chore: Masonを2にアップデート、treesitterを動かす準備をした

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,20 @@
 return {
   {
+    -- Treesitter
     "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      "nvim-treesitter/playground",
+      "windwp/nvim-ts-autotag",
+    },
+    main = "nvim-treesitter.configs",
+    install = function()
+      require("nvim-treesitter.install").setup({
+        prefer_git = false,
+        compilers = { "gcc" },
+      })
+    end,
     opts = {
       ensure_installed = {
         "bash",
@@ -12,6 +26,10 @@ return {
         "markdown_inline",
         "query",
         "sql",
+      },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
       },
     },
     config = function(_, opts)
@@ -25,5 +43,34 @@ return {
       })
       vim.treesitter.language.register("markdown", "mdx")
     end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    event = "CursorMoved",
+  },
+  {
+    "nvim-treesitter/playground",
+    cmd = "TSPlaygroundToggle",
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+    opts = {
+      filetypes = {
+        "html",
+        "javascript",
+        "typescript",
+        "javascriptreact",
+        "typescriptreact",
+        "svelte",
+        "vue",
+        "tsx",
+        "jsx",
+        "xml",
+        "php",
+        "markdown",
+        "mdx",
+      },
+    },
   },
 }
