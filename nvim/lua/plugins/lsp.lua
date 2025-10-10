6945e4e73b2e9e92d1c42f028ecd7e49abb037fb
sasori-256 <Sasori.0274@gmail.com>
2025-10-10 10:16:16 +0900

    feat: add 'config = true' in mason lspconfig

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -9,6 +9,7 @@ return {
   {
     "mason-org/mason-lspconfig.nvim",
     event = { "BufReadPre", "BufNewFile" },
+    config = true,
     dependencies = {
       { "mason-org/mason.nvim" },
       { "neovim/nvim-lspconfig" },
