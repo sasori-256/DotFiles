5975a6c6c2eea495b280e514af1480413ceea3e1
黄泉比良坂46(むつみん) <72808408+sasori-256@users.noreply.github.com>
2026-03-23 15:04:40 +0900

    fix: タイポ入れないで

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -61,7 +61,7 @@ return {
         if (vim.g.colors_name or ""):find("catppuccin") then
           local ok, mod = pcall(require, "catppuccin.special.bufferline")
           if ok then
-            local get = mod.get_theme or mod.get or mod.get_tehme
+            local get = mod.get_theme or mod.get
             if type(get) == "function" then
               opts.highlights = get({})
             end
