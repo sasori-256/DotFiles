f9b6bee276011119092eb27a373bf0b7310624cd
黄泉比良坂46(むつみん) <72808408+sasori-256@users.noreply.github.com>
2026-08-08 01:43:17 +0900

    feat: gitのsshを1passwordで管理するものに寄せた

--- system/darwin/_common/home/programs/git.nix
+++ system/darwin/_common/home/programs/git.nix
@@ -1,12 +1,13 @@
-{ ... }:
+_:
 
 let
   signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHD0yfaaR4biXau0lsvSkungOTqJ0PY0MJ3Y5NXL3yI/";
-  email = "72808408+sasori-256@users.noreply.github.com";
+  noreply-email = "72808408+sasori-256@users.noreply.github.com";
 in
 {
   programs.git = {
     enable = true;
+    lfs.enable = true;
 
     signing = {
       key = signingKey;
@@ -14,10 +15,17 @@ in
     };
 
     settings = {
-      user.name = "黄泉比良坂46(むつみん)";
-      user.email = email;
-      gpg.format = "ssh";
-      gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
+      user = {
+        name = "黄泉比良坂46(むつみん)";
+        email = noreply-email;
+      };
+      gpg = {
+        format = "ssh";
+        ssh = {
+          program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+          allowedSignersFile = "~/.ssh/allowed_signers";
+        };
+      };
     };
   };
 
