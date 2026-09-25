5ed2e9c50a77be569ab470ef5bd24b0103545574
黄泉比良坂46(むつみん) <72808408+sasori-256@users.noreply.github.com>
2026-09-26 01:49:44 +0900

    fix(ssh): depのisDarwinの修正

--- system/_common/home/programs/ssh.nix
+++ system/_common/home/programs/ssh.nix
@@ -2,7 +2,7 @@
 
 let
   agentSocket =
-    if pkgs.stdenv.isDarwin then
+    if pkgs.stdenv.hostPlatform.isDarwin then
       "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
     else
       "~/.1password/agent.sock";
